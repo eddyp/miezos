e2153592ed251330ae9f9b1e86c20497a1be3200
Eddy Petrișor <eddy.petrisor@gmail.com>
2018-08-03 02:05:23 +0300

    Really implement panic_impl via panic_implementation attribute
    The panic_implementation attribute seems to be an unstable feature (i.e. only
    in nightly and could break/disappear in the future) so explicitly request it.
    
    Unfortunately, we still need 'start` lang_item, which seems to have vanished
    with no_std. This kind of makes sense, since main is only special through
    libc/OS support, on baremetal we need a start (typically '_start') symbol to
    have an entry point. On Cortex M, this is written in the addr(vector table)+4.
    
    All this is also explained at https://os.phil-opp.com/freestanding-rust-binary/,
    so it will be interesting to see how does the "Embedded in Rust" instructions
    and implementation of STM32/ARM Cortex M quickstart match with the baremetal
    x86 instructions.
    
    So next step, fix 'start' and then see what's the quickstart about:
    http://blog.japaric.io/quickstart/
    
    Given the current better understanding of asm, it might make sense to try to
    use inline assembly inside Rust, at least to implement a first vector table and
    some startup code:
    https://doc.rust-lang.org/unstable-book/language-features/asm.html
    
    Linker scripts seem to be supported at a basic level via Cortex M support
    (memory.x is the linker script file):
    http://blog.japaric.io/quickstart/#the-cargo-project-template
    and even suggests some prevention of .data/.bss correpution thourgh stack or
    heap growth via rearranging the sections in memory:
    http://blog.japaric.io/stack-overflow-protection/#fixing-it
    
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 #![no_std]
+#![feature(panic_implementation)]
+
+use core::panic::PanicInfo;
+
+/// Function called on panic
+#[panic_implementation]
+#[no_mangle]
+pub fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
 fn main() {
 }
